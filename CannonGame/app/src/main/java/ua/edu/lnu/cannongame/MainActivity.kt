96f249bf42a0504f103bcf57d7010bcae1762e35
den <denny1301@gmail.com>
2020-04-28 01:59:57 +0300

    Created android project with empty activity

---
+++ CannonGame/app/src/main/java/ua/edu/lnu/cannongame/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.edu.lnu.cannongame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
